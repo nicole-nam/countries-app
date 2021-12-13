a1514bea65b3d0dfd1bae73825c868b2cf107d88
nicole-nam <74622899+nicole-nam@users.noreply.github.com>
2021-12-13 10:14:49 -0500

    uiux: install font awesome and add dark mode icon

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
-  <div>hello</div>
+  <the-header></the-header>
 </template>
 
 <script>
+import TheHeader from "./components/layout/TheHeader.vue";
+
 export default {
-  name: "App",
+  components: {
+    TheHeader,
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&family=Space+Mono:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <h1 class="padding">Where in the world?</h1>
+    <div class="padding">
+      <i class="far fa-moon"></i>
+      <p>Dark Mode</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.header {
+  width: 100%;
+  height: 5rem;
+  background-color: #fff;
+  box-shadow: 0 0.3px 10px 0.1px rgb(209, 209, 209);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-family: "Nunito Sans", sans-serif;
+}
+
+.padding {
+  padding: 50px;
+}
+</style>
