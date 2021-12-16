a133988da50565840a8df80232f3646b801df504
nicole-nam <74622899+nicole-nam@users.noreply.github.com>
2021-12-16 17:03:38 -0500

    uiux: add initial grid

--- src/components/ui/CountryCard.vue
+++ src/components/ui/CountryCard.vue
@@ -1,11 +1,13 @@
 <template>
-  <div v-for="(country, index) in list" :key="index">
-    <div class="card">
-      <img :src="country.flags.svg" alt="Avatar" style="width: 100%" />
-      <div class="container">
-        <h4>
-          <b> {{ country.name.common }}</b>
-        </h4>
+  <div class="grid">
+    <div v-for="(country, index) in list" :key="index">
+      <div class="card">
+        <img :src="country.flags.svg" alt="Avatar" style="width: 100%" />
+        <div class="container">
+          <h4>
+            <b> {{ country.name.common }}</b>
+          </h4>
+        </div>
       </div>
     </div>
   </div>
@@ -29,6 +31,11 @@ export default {
 </script>
 
 <style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+}
+
 .card {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
