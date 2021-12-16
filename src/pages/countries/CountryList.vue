e087575b049da45f6cb137abdabe716125bc2900
nicole-nam <74622899+nicole-nam@users.noreply.github.com>
2021-12-16 12:29:38 -0500

    uiux: make initial call to countries api using axios

--- src/pages/countries/CountryList.vue
+++ src/pages/countries/CountryList.vue
@@ -5,6 +5,18 @@
   </div>
 </template>
 
+<script>
+import axios from "axios";
+
+export default {
+  mounted() {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => console.log("response", response));
+  },
+};
+</script>
+
 <style scoped>
 .container {
   padding: 50px 70px;
