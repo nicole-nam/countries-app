1c6d93b97d8f98d7105b713c2d90332a1a24ed76
nicole-nam <74622899+nicole-nam@users.noreply.github.com>
2021-12-17 09:39:26 -0500

    uiux: add gap and width for the country card

--- src/components/ui/CountryCard.vue
+++ src/components/ui/CountryCard.vue
@@ -34,12 +34,13 @@ export default {
 .grid {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  gap: 50px;
 }
 
 .card {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
-  width: 40%;
+  width: 100%;
 }
 
 .container {
